683a2244a7b0a3ed09a65d53479f1eaf85bfaeb8
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2024-07-20 00:28:40 +0700

    initial commit

---
+++ AnggotaAplikasi/AnggotaApp/formAnggota.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formAnggota
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.dataGridAnggota = New System.Windows.Forms.DataGridView()
+		CType(Me.dataGridAnggota,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.SuspendLayout
+		'
+		'dataGridAnggota
+		'
+		Me.dataGridAnggota.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.dataGridAnggota.Location = New System.Drawing.Point(8, 56)
+		Me.dataGridAnggota.Name = "dataGridAnggota"
+		Me.dataGridAnggota.Size = New System.Drawing.Size(784, 384)
+		Me.dataGridAnggota.TabIndex = 0
+		'
+		'formAnggota
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.dataGridAnggota)
+		Me.Name = "formAnggota"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Anggota"
+		CType(Me.dataGridAnggota,System.ComponentModel.ISupportInitialize).EndInit
+		Me.ResumeLayout(false)
+
+End Sub
+
+	Friend WithEvents dataGridAnggota As DataGridView
+End Class
---
+++ AnggotaAplikasi/AnggotaApp/formAnggota.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.Odbc
+
+Public Class formAnggota
+	Dim connect As OdbcConnection
+	Dim dataAdapt As OdbcDataAdapter
+	Dim dataSet As DataSet
+	Dim myDB As String
+
+	Sub connecting()
+		'Cara lain
+		'myDB = "DSN=db32local;Uid=rangga;Pwd=rangga;"
+		myDB = "Driver={MySQL ODBC 8.0 Unicode Driver};Database=db_aplikasi;Server=localhost;User=rangga;Password=rangga;Option=3;"
+		connect = New OdbcConnection(myDB)
+		If connect.State = ConnectionState.Closed Then connect.Open()
+
+		'cara lain
+		'Using connection As New OdbcConnection(myDB)
+           ' Try
+               ' connection.Open()
+             '   Console.WriteLine("Koneksi berhasil!")
+          '  Catch ex As OdbcException
+              '  Console.WriteLine("Terjadi kesalahan: " & ex.Message)
+          '  End Try
+        'End Using
+		
+
+	End Sub
+
+	Private Sub formAnggota_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Call connecting()
+		dataAdapt = New OdbcDataAdapter("SELECT * FROM anggota",connect)
+		dataSet = New DataSet
+		dataSet.Clear()
+		dataAdapt.Fill(dataSet, "anggota")
+		dataGridAnggota.DataSource = (dataSet.Tables("anggota"))
+
+	End Sub
+End Class
